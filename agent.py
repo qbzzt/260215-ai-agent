1b62e2a261c4248c5d8c264302807e0f195bc28b
Ori Pomerantz <qbzzt@gmail.com>
2026-01-26 20:53:46 -0600

    Read quotes

---
+++ agent.py
@@ -0,0 +1,166 @@
+from web3 import Web3
+from web3.contract import Contract
+from decimal import Decimal, ROUND_HALF_UP
+from dataclasses import dataclass
+from datetime import datetime, timezone, timedelta
+from pprint import pprint
+import time
+import functools
+import sys
+
+print = functools.partial(print, flush=True)
+
+MAINNET_URL = "https://eth.drpc.org"
+BLOCK_TIME_SECONDS = 12
+MINUTE_BLOCKS = int(60 / BLOCK_TIME_SECONDS)
+HOUR_BLOCKS = MINUTE_BLOCKS * 60
+DAY_BLOCKS = HOUR_BLOCKS * 24
+CYCLE_BLOCKS = DAY_BLOCKS
+
+# The address of the pool we're reading
+WETHUSDC_ADDRESS = Web3.to_checksum_address("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640")
+
+
+POOL_ABI = [
+    {
+        "inputs": [],
+        "name": "slot0",
+        "outputs": [
+            {"internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160"},
+            {"internalType": "int24", "name": "tick", "type": "int24"},
+            {"internalType": "uint16", "name": "observationIndex", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinality", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinalityNext", "type": "uint16"},
+            {"internalType": "uint8", "name": "feeProtocol", "type": "uint8"},
+            {"internalType": "bool", "name": "unlocked", "type": "bool"},
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token0",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token1",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
+
+ERC20_ABI = [
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {"name": "", "type": "string"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {"name": "", "type": "uint8"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+w3 = Web3(Web3.HTTPProvider(MAINNET_URL))
+
+@dataclass(frozen=True)
+class ERC20Token:
+    address: str
+    symbol: str
+    decimals: int
+    contract: Contract
+
+    
+@dataclass(frozen=True)
+class PoolInfo:
+    address: str
+    token0: ERC20Token
+    token1: ERC20Token
+    contract: Contract
+    asset: str
+    decimal_factor: Decimal = 1
+
+    def get_price(self, block: int = w3.eth.block_number) -> Decimal:
+        assert block <= w3.eth.block_number, "Block is in the future"
+        sqrt_price_x96 = Decimal(self.contract.functions.slot0().call(block_identifier=block)[0])
+        raw_price = (sqrt_price_x96 / Decimal(2**96)) ** 2  # (token1 per token0)
+        return 1/(raw_price * self.decimal_factor)
+
+@dataclass(frozen=True)
+class Quote:
+    timestamp: str
+    price: Decimal
+    asset: str
+
+
+def read_token(address: str) -> ERC20Token:
+    token = w3.eth.contract(address=address, abi=ERC20_ABI)
+    symbol = token.functions.symbol().call()
+    decimals = token.functions.decimals().call()
+
+    return ERC20Token(
+        address=address, 
+        symbol=symbol,
+        decimals=decimals,
+        contract=token
+    )
+
+def read_pool(address: str) -> PoolInfo:
+    pool_contract = w3.eth.contract(address=address, abi=POOL_ABI)
+    token0Address = pool_contract.functions.token0().call()
+    token1Address = pool_contract.functions.token1().call()
+    token0 = read_token(token0Address)
+    token1 = read_token(token1Address)
+    return PoolInfo(
+        address=address,
+        asset=f"{token1.symbol}/{token0.symbol}",
+        token0=token0,
+        token1=token1,
+        contract=pool_contract,
+        decimal_factor=Decimal(10) ** Decimal(token0.decimals - token1.decimals)
+    )
+
+def get_quote(pool: PoolInfo, block_number: int = None) -> Quote:
+    if block_number is None:
+        block_number = w3.eth.block_number
+    block = w3.eth.get_block(block_number)
+    price = pool.get_price(block_number)
+    return Quote(
+        timestamp=datetime.fromtimestamp(block.timestamp, timezone.utc).isoformat(),
+        price=price.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP),
+        asset=pool.asset
+    )
+
+def get_quotes(pool: PoolInfo, start_block: int, end_block: int, step: int) -> list[Quote]:
+    quotes = []
+    for block in range(start_block, end_block + 1, step):
+        print(f"Fetching quote for block {block}...")
+        quote = get_quote(pool, block)
+        quotes.append(quote)
+    return quotes
+
+pool = read_pool(WETHUSDC_ADDRESS)
+quotes = get_quotes(
+    pool, 
+    w3.eth.block_number - 12*CYCLE_BLOCKS, 
+    w3.eth.block_number, 
+    CYCLE_BLOCKS
+)
+
+pprint(quotes)
